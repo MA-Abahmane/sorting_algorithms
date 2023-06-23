0c8343585789e0f5abf4d7d935871f31ca8723d3
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-23 10:56:18 +0000

    done: Insertion sort algorithm for double linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,9 +8,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *copy;
-	if (!(*list) || !(*list)->next)
-		return;
-	copy = (*list);
+listint_t *copy;
+if (!(*list) || !(*list)->next)
+	return;
+copy = (*list);
+swap_values(copy, copy->next);
+while (copy)
+{
+	if (copy->prev && copy->prev->n > copy->n)
+	{
+		swap_values(copy->prev, copy);
+		print_list(*list);
+		copy = copy->prev;
+	}
+	else
+		copy = copy->next;
+}
+}
 
+/**
+ * swap_values - Swap the value of two elements
+ * of a double linked list
+ * @first: First element
+ * @second: Second element
+ * Return: void
+ */
+void swap_values(listint_t *first, listint_t *second)
+{
+	int value = first->n;
+	*(int *)&first->n = second->n;
+	*(int *)&second->n = value;
 }
