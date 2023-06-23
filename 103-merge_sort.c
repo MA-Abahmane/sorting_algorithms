070ea3502cf7a7b118e50ef72e58ca0d0aa78e0c
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-23 23:11:12 +0000

    init: Merge sort algorithm

---
+++ 103-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * merge_sort - Implementation of the
+ * merge sort algorithm
+ * @array: Pointer to int array
+ * @size: Array size
+ * Return void
+*/
+void merge_sort(int *array, size_t size)
+{
+
+}
--- sort.h
+++ sort.h
@@ -36,6 +36,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
 
 /* Advanced: */
 void shell_sort(int *array, size_t size);
