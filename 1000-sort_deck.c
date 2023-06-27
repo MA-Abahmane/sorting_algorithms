dad28455a8b174cfbfadd25cbf799ffdf96c5556
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-27 14:39:56 +0100

    Sorting-1000

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,5 +1,6 @@
 #include "deck.h"
 
+
 /**
  * sort_deck - sorts a DLL of integers in
  * ascending order using the insertion sort
@@ -9,9 +10,28 @@
  * Return: no return
  */
 void sort_deck(deck_node_t **deck)
-{
+{   
+    int s, r;
+    char kinds[4] = {'S', 'H', 'C', 'D'};
 
-if (!(*deck) || !(*deck)->next || !deck)
-return;
+    kind_t suits[4] = {SPADE, HEART, CLUB, DIAMOND};
+    const char *rank[13] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
+
+    if (!(*deck)->next || !deck)
+        return;
+
+
+    for (s = 0; s < 4; s++)
+    {
+        for (r = 0; r < 13; r++)
+        {   
+            if (r)
+                printf(", ");
+            printf("{%s, %c}", rank[r], kinds[suits[s]]);
+        }
+        if ( s < 3)
+            printf("\n");
+    }
+    *deck = NULL;
 }
